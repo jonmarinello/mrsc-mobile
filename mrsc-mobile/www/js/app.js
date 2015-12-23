bd52860ec08c38f55916938c090ecade0eb19a25
Jon Marinello <jonmarinello@gmail.com>
2015-12-23 03:50:04 -0800

    General design refactoring and code cleanup.

--- mrsc-mobile/www/js/app.js
+++ mrsc-mobile/www/js/app.js
@@ -1,102 +1,103 @@
 // Ionic MRSC App
-angular.module('mrsc', ['ionic', 'mrsc.controllers', 'ngMessages'])
+angular.module('mrsc', ['ionic', 'mrsc.controllers', 'mrsc.factories', 'ngMessages'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'templates/tabs.html'
-    })
-
-    // Each tab has its own nav history stack:
-    .state('tab.home', {
-      url: '/home',
-      views: {
-        'tab-home': {
-          templateUrl: 'templates/tab-home.html',
-          controller: 'HomeCtrl'
-        }
       }
-    })
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-    .state('tab.services', {
-        url: '/services',
+
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      // Each tab has its own nav history stack:
+      .state('tab.home', {
+        url: '/home',
         views: {
-          'tab-services': {
-            templateUrl: 'templates/tab-services.html',
-            controller: 'ServicesCtrl'
+          'tab-home': {
+            templateUrl: 'templates/tab-home.html',
+            controller: 'HomeCtrl'
           }
         }
       })
 
-    .state('tab.about', {
-      url: '/about',
-      views: {
-        'tab-about': {
-          templateUrl: 'templates/tab-about.html',
-          controller: 'AboutCtrl'
+      .state('tab.services', {
+          url: '/services',
+          views: {
+            'tab-services': {
+              templateUrl: 'templates/tab-services.html',
+              controller: 'ServicesCtrl'
+            }
+          }
+        })
+
+      .state('tab.about', {
+        url: '/about',
+        views: {
+          'tab-about': {
+            templateUrl: 'templates/tab-about.html',
+            controller: 'AboutCtrl'
+          }
         }
-      }
-    })
+      })
 
-    .state('tab.portfolio', {
-      url: '/portfolio',
-      views: {
-        'tab-portfolio': {
-          templateUrl: 'templates/tab-portfolio.html',
-          controller: 'PortfolioCtrl'
+      .state('tab.portfolio', {
+        url: '/portfolio',
+        views: {
+          'tab-portfolio': {
+            templateUrl: 'templates/tab-portfolio.html',
+            controller: 'PortfolioCtrl'
+          }
         }
-      }
-    })
+      })
 
-    .state('tab.contact', {
-      url: '/contact',
-      views: {
-        'tab-contact': {
-          templateUrl: 'templates/tab-contact.html',
-          controller: 'ContactCtrl'
+      .state('tab.contact', {
+        url: '/contact',
+        views: {
+          'tab-contact': {
+            templateUrl: 'templates/tab-contact.html',
+            controller: 'ContactCtrl'
+          }
         }
-      }
-    })
+      })
 
-    .state('tab.start-a-project', {
-      url: '/start-a-project',
-      views: {
-        'tab-start-a-project': {
-          templateUrl: 'templates/tab-start-a-project.html',
-          controller: 'StartAProjectCtrl'
+      .state('tab.start-a-project', {
+        url: '/start-a-project',
+        views: {
+          'tab-start-a-project': {
+            templateUrl: 'templates/tab-start-a-project.html',
+            controller: 'StartAProjectCtrl'
+          }
         }
-      }
-    });
+      });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/home');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/home');
 
-})
+  })
 
-//.constant('DOMAIN', 'http://localhost:3000');
-//.constant('DOMAIN', 'http://staging.missionridgeconsulting.com');
-.constant('DOMAIN', 'http://www.missionridgeconsulting.com');
+  //.constant('DOMAIN', 'http://localhost:3000');
+  //.constant('DOMAIN', 'http://staging.missionridgeconsulting.com');
+  .constant('DOMAIN', 'http://www.missionridgeconsulting.com');
